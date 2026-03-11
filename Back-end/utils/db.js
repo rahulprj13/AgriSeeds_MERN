1059296b18684f3dcb6521eca771782e31a825e6
Rahul Prajapati <156348147+rahulprj13@users.noreply.github.com>
2026-03-11 12:23:27 +0530

    dbconnection

--- Back-end/utils/db.js
+++ Back-end/utils/db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose")
 const dotenv = require("dotenv")
 dotenv.config()
+
 const connectDB = async ()=>{
     try{
         await mongoose.connect(process.env.MONGO_URL)
