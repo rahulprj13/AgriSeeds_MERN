15b99b8ce330fe39a3a16e64dac4d27fe57e23df
Rahul Prajapati <156348147+rahulprj13@users.noreply.github.com>
2026-03-16 21:45:54 +0530

    create cartRoutes

--- Back-end/routes/CartRoutes.js
+++ Back-end/routes/CartRoutes.js
@@ -1,69 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const Cart = require("../models/CartModel.js");
+const cartController = require("../controllers/cartController.js");
 
-
-// add item
-router.post("/add", async (req, res) => {
-
-  const { userId, product } = req.body;
-
-  const newItem = new Cart({
-    userId,
-    productId: product._id,
-    name: product.name,
-    price: product.price,
-    image: product.image,
-    quantity: 1
-  });
-
-  await newItem.save();
-
-  res.json({
-    message: "Item added",
-    data: newItem
-  });
-
-});
-
-
-// get cart
-router.get("/:userId", async (req, res) => {
-
-  const cart = await Cart.find({ userId: req.params.userId });
-
-  res.json({
-    data: cart
-  });
-
-});
-
-
-// update quantity
-router.put("/:id", async (req, res) => {
-
-  const { quantity } = req.body;
-
-  const item = await Cart.findByIdAndUpdate(
-    req.params.id,
-    { quantity },
-    { new: true }
-  );
-
-  res.json(item);
-
-});
-
-
-// remove item
-router.delete("/:id", async (req, res) => {
-
-  await Cart.findByIdAndDelete(req.params.id);
-
-  res.json({
-    message: "Item removed"
-  });
-
-});
+router.post("/add", cartController.addToCart);
+router.get("/:userId", cartController.getCart);
+router.put("/update/:id", cartController.updateQuantity);
+router.delete("/remove/:id", cartController.removeItem);
 
 module.exports = router;
