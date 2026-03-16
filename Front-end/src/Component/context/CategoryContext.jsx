08f7974c2111536b8c33a4c8c951e9f3ba85513e
Rahul Prajapati <156348147+rahulprj13@users.noreply.github.com>
2026-03-16 19:44:25 +0530

    set api

--- Front-end/src/Component/context/CategoryContext.jsx
+++ Front-end/src/Component/context/CategoryContext.jsx
@@ -15,7 +15,7 @@ const CategoryContextProvider = ({ children }) => {
 
     try {
 
-      const res = await axios.get("/api/admin/categories");
+      const res = await axios.get("/api/categories");
 
       setCategories(res.data || []);
 
