d7399788fe5de0db30d7537f835ef75324df78ba
Rahul Prajapati <156348147+rahulprj13@users.noreply.github.com>
2026-03-25 18:21:05 +0530

    add totalOrders

--- Back-end/routes/AdminDashboardRoutes.js
+++ Back-end/routes/AdminDashboardRoutes.js
@@ -18,12 +18,14 @@ router.get(
   adminMiddleware,
   async (req, res) => {
     try {
-      const [totalUsers, totalProducts, totalCategories, totalCartItems] =
+      const [totalUsers, totalProducts, totalCategories, totalCartItems, totalOrders] =
         await Promise.all([
           User.countDocuments(),
           Product.countDocuments(),
           Category.countDocuments(),
           Cart.countDocuments(),
+          Order.countDocuments(),
+
         ]);
 
       res.json({
@@ -31,6 +33,7 @@ router.get(
         totalProducts,
         totalCategories,
         totalCartItems,
+        totalOrders
       });
     } catch (err) {
       res.status(500).json({ message: "Server error" });
